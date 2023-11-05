5f7a90346604d3b8978a42a3cf23beba917f6a2b
M4T1SS3 <markorester@gmail.com>
2023-11-05 21:38:59 +0100

    huh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-'use client';
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
+import { useRouter } from 'next/router'; // Corrected import
 
 export default function Home() {
   const [dataUploaded, setDataUploaded] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-      router.push('/menu');
-  }, []);
-
+    router.push('/menu');
+  }, [router]); // Corrected dependency array
 
   return (
     <main className=" w-screen grid place-items-center">
